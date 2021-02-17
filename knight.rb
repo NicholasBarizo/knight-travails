d784ed5691ac84e8152c8608c312de1b7dd9007d
Nicholas <nicholasbarizo@gmail.com>
2021-02-16 23:29:59 -0800

    will diplay path if destination is 1 move away

--- knight.rb
+++ knight.rb
@@ -12,20 +12,52 @@ class Knight
     @up_left = up_left
   end
   def move
-    @up_right 
+    @up_right
   end
 end
 
-def knight_moves(start, finish, depth=0)
+def knight_moves(start, finish)
+  if start == finish
+    puts "The knight doesn't need to travel"
+    exit
+  end
   moves = [[1, 2], [2, 1], [2, -1], [1, -2], [-1, -2], [-2, -1], [-2, 1], [-1, 2]]
-  best = [0, 0]
-  moves.each_with_index do |move, i|
-    best[0] = knight_moves([start[0 + move[0]], start[1 + move[1]]], finish, depth + 1) if is_valid?(move)
+  queue = Array.new [start]
+  p queue
+  queue.each do |item|
+    next if item.is_a? Integer
+
+    p "queue: #{queue} item: #{item}"
+    moves.each do |move|
+      p "move: #{move}"
+      new_move = [item[0] + move[0], item[1] + move[1]]
+      next unless valid?(new_move)
+
+      puts "#{new_move}"
+      reach_end(start, new_move) if new_move == finish
+      # modded_item = 
+      queue.push [item[0], new_move]
+    end
   end
 end
 
-def is_valid?(move)
-  move[0] < 1 || move[0] > 8 || move[1] < 1 || move[1] > 8 ? false : true
+public
+
+def valid?(move)
+  move[0].between?(1, 8) && move[1].between?(1, 8) ? true : false
+end
+
+def reach_end(start, finish)
+  if start.flatten.length == 2
+    start = [start, finish]
+  else
+    start.push finish
+  end
+  puts "The knight reaches its destination in #{start.length - 1} move#{'s' if start.length == 1}. Here's its trail:"
+  start.each_with_index do |move, ind|
+    p move
+  end
+  exit
 end
 
 def create_board
@@ -65,4 +97,4 @@ def create_board
   puts "       A       B       C       D       E       F       G       H\n"
 end
 
-create_board
+knight_moves([3, 7], [2, 5])
