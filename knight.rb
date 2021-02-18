ed2ffe3e5ef28e5b38f9c442f3562196aa78bbab
Nicholas <nicholasbarizo@gmail.com>
2021-02-18 12:29:24 -0800

    Shows knight traversal on grid

--- knight.rb
+++ knight.rb
@@ -17,7 +17,6 @@ class Knight
 end
 
 def knight_moves(start, finish)
-  
   require 'colorize'
   if start == finish
     puts "The knight doesn't need to travel"
@@ -25,9 +24,7 @@ def knight_moves(start, finish)
   end
   moves = [[1, 2], [2, 1], [2, -1], [1, -2], [-1, -2], [-2, -1], [-2, 1], [-1, 2]]
   queue = Array.new [start]
-  p queue
   until queue.empty?
-    queue.each { |item| puts "#{item}".light_red }
     item = queue[0]
 
     if item.flatten.length == 2
@@ -35,19 +32,10 @@ def knight_moves(start, finish)
     else
       current_item = item[-1]
     end
-    if item.is_a? Integer
-      next
-      queue.shift
-    end
-
-    puts "item: #{item}".red
-    puts "current #{current_item}".yellow
+    next if item.is_a? Integer
     moves.each do |move|
-      puts "modifier: #{move}".cyan
       new_move = [current_item[0] + move[0], current_item[1] + move[1]]
       next if item.any? { |coord| coord == new_move }
-
-      puts "new move #{new_move}".green
       next unless valid?(new_move)
 
       reach_end(item, new_move) if new_move == finish
@@ -75,7 +63,7 @@ def reach_end(start, finish)
   end
   coords = convert_to_coords(start)
   create_board(start)
-  puts "The knight reaches its destination in #{start.length - 1} move#{'s' if start.length == 1}. Here's its trail:"
+  puts "The knight reaches its destination in #{start.length - 1} move#{'s' if start.length > 1}. Here is its trail:"
   coords.each_with_index do |coord, ind|
     pos = "Move #{ind}"
     pos = 'Start' if ind == 0
@@ -85,19 +73,21 @@ def reach_end(start, finish)
 end
 
 def convert_to_coords(start)
-  start.each do |coord|
-    coord[0] = case coord[0]
-               when 1 then 'A'
-               when 2 then 'B'
-               when 3 then 'C'
-               when 4 then 'D'
-               when 5 then 'E'
-               when 6 then 'F'
-               when 7 then 'G'
-               when 8 then 'H'
+  coord = []
+  start.each_with_index do |move, ind|
+    coord.push case move[0]
+               when 1 then ['A', move[1]]
+               when 2 then ['B', move[1]]
+               when 3 then ['C', move[1]]
+               when 4 then ['D', move[1]]
+               when 5 then ['E', move[1]]
+               when 6 then ['F', move[1]]
+               when 7 then ['G', move[1]]
+               when 8 then ['H', move[1]]
                end
+    coord[ind][1] = move[1]
   end
-  start
+  coord
 end
 
 def create_board(start)
@@ -113,13 +103,20 @@ def create_row(row, coords)
   puts '   |       |       |       |       |       |       |       |       |'
   print " #{row} |"
   8.times do |column|
-    create_cell(row, column, coords)
+    create_cell(row, column + 1, coords)
   end
   puts "\n   |       |       |       |       |       |       |       |       |"
 end
 
 def create_cell(row, column, coords)
-  print "   #{row}   |"
+  move = '       '
+  pos = [column, row]
+  coords.each_with_index do |coord, ind|
+    move = "   #{ind}   " if pos == coord
+    move = ' Start ' if ind.zero? && pos == coord
+    move = 'Finish ' if ind == coords.length - 1 && pos == coord
+  end
+  print "#{move}|"
 end
 
-knight_moves([2, 2], [1, 1])
+knight_moves([8, 8], [1, 1])
